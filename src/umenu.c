d70f320661b4a32f9c46a1cb93403eb1f3f3c417
Jacek Wieczorek <mrjjot@gmail.com>
2017-10-28 14:52:29 +0200

    Add routines for basic menu browsing (up, down)

--- src/umenu.c
+++ src/umenu.c
@@ -3,24 +3,80 @@
 
 static const struct umenunode *umenuNext( struct umenu *menu )
 {
-	int i, depth;
+	struct umenunode *entry;
+	int depth;
 
-	if ( menu == NULL ) return NULL;
+	//Validate all pointers
+	if ( menu == NULL || menu->tree == NULL || menu->current == NULL ) return NULL;
 
-	depth = menu->tree[menu->current].depth;
+	//Remember the depth we are looking for
+	depth = menu->current->depth;
 
-	for ( i = menu->current + 1; i < menu->treesize; i++ )
+	//Iterate through the nodes
+	for ( entry = menu->current + 1; entry < menu->tree + menu->treesize; entry++ )
 	{
-		if ( menu->tree[i].depth == depth )
+		//The node we found becomes the current one
+		if ( entry->depth == depth )
 		{
-			menu->current = i;
-			return &menu->tree[i];
+			menu->current = entry;
+			break;
 		}
 	}
+
+	return menu->current;
+}
+
+static const struct umenunode *umenuPrev( struct umenu *menu )
+{
+	struct umenunode *entry;
+	int depth;
+
+	//Validate all pointers
+	if ( menu == NULL || menu->tree == NULL || menu->current == NULL ) return NULL;
+
+	//Remember the depth we are looking for
+	depth = menu->current->depth;
+
+	//Iterate through the preceding nodes
+	for ( entry = menu->current - 1; entry >= menu->tree; entry-- )
+	{
+		//The node we found becomes the current one
+		if ( entry->depth == depth )
+		{
+			menu->current = entry;
+			break;
+		}
+	}
+
+	return menu->current;
 }
 
 //Interacts with given menu - passes a keypress
 int umenuInteract( struct umenu *menu, int key )
 {
+	if ( menu == NULL ) return 1;
+
+	//Browse menu or change value - depending on whether edit mode is enabled
+	if ( menu->editmode )
+	{
+
+	}
+	else
+	{
+		switch ( key )
+		{
+			case UMENU_KEY_UP:
+				umenuPrev( menu );
+				break;
+
+			case UMENU_KEY_DN:
+				umenuNext( menu );
+				break;
+
+			default:
+				break;
+		}
+	}
+
 	return 0;
 }
