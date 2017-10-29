425b70cd7278b0c0329526aa88f0a1bd59df1440
Jacek Wieczorek <mrjjot@gmail.com>
2017-10-29 02:01:11 +0200

    Update test code

--- examples/demo.c
+++ examples/demo.c
@@ -1,19 +1,23 @@
 #include <stdio.h>
-#include <umenu.h>
+#include <umenu/umenu.h>
+#include <umenu/umenuprint.h>
 #include <ctype.h>
 #include <ncurses.h>
 #include <signal.h>
 #include <string.h>
 #include <stdlib.h>
+#include <stdarg.h>
 
 int alive = 1;
 int clicks = 0;
+WINDOW *lcd;
+WINDOW *lcdframe;
 
 //The menu tree
 struct umenunode menutree[] =
 {
 	{ UMENU_ROOT },
-	{ 0, "clicks", UMENU_INT, &clicks, 0, NULL },
+	//{ 0, "clicks", UMENU_INT, &clicks, 0, NULL },
 	{ 0, "my menu", UMENU_SUB, NULL, 0, NULL },
 		{ 1, "some submenu 0", UMENU_SUB, NULL, 0, NULL },
 		{ 1, "some submenu 1", UMENU_SUB, NULL, 0, NULL },
@@ -27,7 +31,15 @@ struct umenunode menutree[] =
 };
 #define MENU_SIZE ( sizeof( menutree ) / sizeof( menutree[0] ) ) 
 
-
+int lcdprint( int x, int y, const char *format, ... )
+{
+	va_list args;
+	va_start( args, format );
+	wmove( lcd, y, x );
+	vwprintw( lcd, format, args );
+	va_end( args );
+	return 0;
+}
 
 void quit( int dummy )
 {
@@ -44,7 +56,7 @@ int main( int argc, char **argv )
 	int maxx, maxy;
 	
 	struct umenu menu;
-	umenuInit( &menu, menutree, MENU_SIZE, NULL );
+	umenuInit( &menu, menutree, MENU_SIZE, menutree + 2 );
 	
 	signal( SIGINT, quit );
 	
@@ -55,13 +67,13 @@ int main( int argc, char **argv )
 	keypad( stdscr, TRUE );
 	
 	//Create virtual LCD
-	int lcdx = 16, lcdy = 2;
+	int lcdx = 16, lcdy = 4;
 	init_pair( 1, COLOR_WHITE, COLOR_BLUE );
 	init_pair( 2, COLOR_WHITE, COLOR_BLACK );
 	init_pair( 3, COLOR_WHITE, COLOR_WHITE );
 	getmaxyx( stdscr, maxy, maxx );
-	WINDOW *lcdframe = newwin( lcdy + 2, lcdx + 2, ( maxy - lcdy - 2 ) / 2, ( maxx - lcdx - 2 ) / 2 );
-	WINDOW *lcd = newwin( lcdy, lcdx, ( maxy - lcdy ) / 2, ( maxx - lcdx ) / 2 );
+	lcdframe = newwin( lcdy + 2, lcdx + 2, ( maxy - lcdy - 2 ) / 2, ( maxx - lcdx - 2 ) / 2 );
+	lcd = newwin( lcdy, lcdx, ( maxy - lcdy ) / 2, ( maxx - lcdx ) / 2 );
 	wbkgd( stdscr, COLOR_PAIR( 3 ) );
 	wbkgd( lcdframe, COLOR_PAIR( 2 ) );
 	wbkgd( lcd, COLOR_PAIR( 1 ) );
@@ -69,9 +81,11 @@ int main( int argc, char **argv )
 	while ( 1 )
 	{
 		//Update LCD
-		werase( lcd );
+		//werase( lcd );
 		snprintf( buf, 512, "%s", menu.current->header );
-		mvwprintw( lcd, 0, 0, "%s", buf );
+		//mvwprintw( lcd, 0, 0, "%s", buf );
+		umenuPrint( &menu, lcdx, lcdy, 0, 0, lcdprint ); 
+		//lcdprint( 0, 0, "dupa" );
 		mvwprintw( lcdframe, 0, ( lcdx + 2 ) / 2 - 6, "LCD SIM 3000" );
 		wrefresh( stdscr );
 		wrefresh( lcdframe );
