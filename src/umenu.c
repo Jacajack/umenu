f39181a43731636066910bacd2dd03358a8bce07
Jacek Wieczorek <mrjjot@gmail.com>
2017-10-29 02:26:57 +0200

    Remove umenuReturn and umenuEnter - rely on core functions

--- src/umenu.c
+++ src/umenu.c
@@ -2,54 +2,6 @@
 #include <umenu/umenu.h>
 #include <umenu/core.h>
 
-
-
-static const struct umenuentry *umenuEnter( struct umenu *menu )
-{
-	//Validate all pointers
-	if ( menu == NULL || menu->tree == NULL || menu->current == NULL ) return NULL;
-
-	if ( menu->current->vtype == UMENU_SUB )
-	{
-		if ( menu->current[1].depth != UMENU_BOUNDARY && \
-			menu->current[1].depth == menu->current->depth + 1 )
-		{
-			menu->current = menu->current + 1;
-		}
-	}
-	else
-	{
-		if ( menu->current->vtype & UMENU_EDIT )
-			menu->editmode = 1;
-		else menu->watchmode = 1;
-	}
-	return 0;
-}
-
-static const struct umenuentry *umenuReturn( struct umenu *menu )
-{
-	const struct umenuentry *entry;
-
-	//Validate all pointers
-	if ( menu == NULL || menu->tree == NULL || menu->current == NULL ) return NULL;
-
-	if ( menu->editmode )
-		menu->editmode = 0;
-	else if ( menu->watchmode )
-		menu->watchmode = 0;
-	else
-	{
-		entry = umenuParent( menu->current );
-		if ( entry != NULL )
-		{
-			menu->current = entry;
-			return entry;
-		}
-	}
-
-	return 0;
-}
-
 //Interacts with given menu - passes a keypress
 int umenuInteract( struct umenu *menu, int key )
 {
@@ -66,20 +18,23 @@ int umenuInteract( struct umenu *menu, int key )
 	{
 		switch ( key )
 		{
+			//Move up
 			case UMENU_KEY_UP:
 				entry = umenuPrev( menu->current );
 				if ( entry != NULL ) menu->current = entry;
 				break;
 
+			//Move down
 			case UMENU_KEY_DN:
 				entry = umenuNext( menu->current );
 				if ( entry != NULL ) menu->current = entry;
 				break;
 
+			//Go one level deeper or edit value
 			case UMENU_KEY_ENTER:
 				if ( menu->current->vtype == UMENU_SUB )
 				{
-					entry = umenuEnter( menu );
+					entry = umenuChild( menu->current );
 					if ( entry != NULL ) menu->current = entry;
 				}
 				else if ( menu->current->vtype & UMENU_EDIT ) menu->editmode = 1;
@@ -87,10 +42,20 @@ int umenuInteract( struct umenu *menu, int key )
 
 				break;
 
+			//Go one level up
 			case UMENU_KEY_RETURN:
-				umenuReturn( menu );
+				if ( menu->editmode )
+					menu->editmode = 0;
+				else if ( menu->watchmode )
+					menu->watchmode = 0;
+				else
+				{
+					entry = umenuParent( menu->current );
+					if ( entry != NULL ) menu->current = entry;
+				}
 				break;
 
+			//Unknown keys - should result with an error in future
 			default:
 				break;
 		}
